4ae47bb7a7673095e5bb462d938a94ca2891709e
faizan2700 <syedfaizan824@gmail.com>
2024-05-08 07:02:14 +0530

    updated code

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask 
 import azure.cosmos.cosmos_client as cosmos_client 
+from dotenv import load_dotenv 
+load_dotenv() 
+import os 
 
 app = Flask(__name__) 
 
 HOST = 'https://third-party-data-store1.documents.azure.com:443/'
 DATABASE_ID = 'database1'
-CONTAINER_ID = 'example1'
+CONTAINER_ID = 'example1' 
 
 def run_sample():
-    client = cosmos_client.CosmosClient(HOST, {'masterKey': 'm4MQPRRqNNt32VLnbRedud4zMjeWwjCuQozLLV9rr7GyLsfQDP0D5CVN6oRdlAychwAM1d3UoP8iACDbc6WUrg=='}, user_agent="CosmosDBPythonQuickstart", user_agent_overwrite=True)
+    client = cosmos_client.CosmosClient(HOST, {'masterKey': os.getenv('MASTER_KEY')}, user_agent_overwrite=True)
     # try:
     #     # setup database for this sample
     #     try:
