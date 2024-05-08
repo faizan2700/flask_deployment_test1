5610a0fed2f037069612f64ae2650a65916d2038
faizan2700 <syedfaizan824@gmail.com>
2024-05-08 07:23:32 +0530

    updated code

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask 
-
+import azure.cosmos.cosmos_client as cosmos_client 
+from dotenv import load_dotenv 
+load_dotenv() 
+import os 
 
 app = Flask(__name__) 
 
+HOST = os.getenv('HOST') 
+DATABASE_ID = os.getenv('DATABASE_ID') 
+CONTAINER_ID = os.getenv('CONTAINER_ID') 
+MASTER_KEY = os.getenv('MASTER_KEY')
+
+def run_sample():
+    client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY}, user_agent_overwrite=True)
  
 @app.route('/') 
 def hello_world(): 
+    run_sample() 
     return '<h1>Hello World</h1>' 
