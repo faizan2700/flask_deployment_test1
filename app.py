dec5ab5b4c6fc9c0e5236ee4acf2942e31c616b5
faizan2700 <syedfaizan824@gmail.com>
2024-05-08 07:36:17 +0530

    updated code

--- app.py
+++ app.py
@@ -6,14 +6,15 @@ import os
 
 app = Flask(__name__) 
 
-HOST = 'https://third-party-data-store1.documents.azure.com:443/'
-DATABASE_ID = 'database1' 
-CONTAINER_ID = 'example1' 
-MASTER_KEY = 'm4MQPRRqNNt32VLnbRedud4zMjeWwjCuQozLLV9rr7GyLsfQDP0D5CVN6oRdlAychwAM1d3UoP8iACDbc6WUrg=='
+# HOST = 'https://third-party-data-store1.documents.azure.com:443/'
+# DATABASE_ID = 'database1' 
+# CONTAINER_ID = 'example1' 
+# MASTER_KEY = 'm4MQPRRqNNt32VLnbRedud4zMjeWwjCuQozLLV9rr7GyLsfQDP0D5CVN6oRdlAychwAM1d3UoP8iACDbc6WUrg=='
 
 def run_sample():
-    client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY}, user_agent_overwrite=True)
- 
+    # client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY}, user_agent_overwrite=True)
+    pass 
+
 @app.route('/') 
 def hello_world(): 
     run_sample() 
